d7fbcd1b9baeea2924acfb696e0cf72badee201b
Ruike Lyu <47710448+Rick10119@users.noreply.github.com>
2020-08-22 12:09:45 +0800

    Add files via upload
    MyLove used to be Java, now Java knows who I love

---
+++ MyLove.java
@@ -0,0 +1,15 @@
+public class MyLove {
+    public static void searchForYou () {
+        // 我的爱只有一种方法，那就是找你
+        foreach(you in world) {
+
+            // 如果我找到了tt
+            if(you == "tt") {
+
+                // 你作为另一半，就让我的心完整
+                myHeart.theOtherHalf = you;
+            }
+        }
+
+    }
+}
